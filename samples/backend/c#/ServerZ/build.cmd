8fd1cdfdd2caff7134663c0fbc2ab114bcff2466
neo36 <neo365@gmail.com>
2023-02-23 20:13:17 +0900

    delete

--- samples/backend/c#/ServerZ/build.cmd
+++
@@ -1,5 +0,0 @@
-rmdir /s /q dist
-
-dotnet clean
-dotnet publish -r win-x64 /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true --output .\dist\WithSDK --self-contained true
-dotnet publish -r win-x64 /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true --output .\dist\WithoutSDK --no-self-contained
