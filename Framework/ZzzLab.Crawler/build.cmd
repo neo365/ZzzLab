0b880a5b7eecd65419fbbc321bbca74d815da0e5
neo36 <neo365@gmail.com>
2023-02-23 12:35:59 +0900

    ZzzLab.Crawler init

---
+++ Framework/ZzzLab.Crawler/build.cmd
@@ -0,0 +1,47 @@
+@echo off
+SET /a dt=%DATE:-=% + 1010
+SET yy=%dt:~2,2%
+SET mm=%dt:~4,2%
+SET dd=%dt:~6,2%
+SET /a hh=%TIME:~0,2% + 10
+SET min=%TIME:~3,2%
+
+SET version=0.21%yy%.%mm%%dd%.%hh%%min%
+
+IF NOT "%1" == "" SET version=%1
+
+echo ===============================
+echo %version% 
+echo ===============================
+
+del *.bak
+
+cd .\src
+
+dotnet list package
+
+dotnet add package HtmlAgilityPack
+dotnet add package ZzzLab.Core
+
+dotnet list package
+
+cd ..
+git commit -a -m "Auto Build: "%version% &
+git push --progress
+
+cd .\src
+
+dotnet clean
+dotnet publish -p:Version=%version%
+dotnet pack --output ..\..\..\Package -p:Version=%version%
+cd ..
+
+IF NOT "%2" == "" (
+    SET ApiToken=%2
+    dotnet nuget push ..\..\Package\ZzzLab.Crawler.%version%.nupkg --api-key %ApiToken% --source https://api.nuget.org/v3/index.json
+    start chrome https://www.nuget.org/packages/ZzzLab.Crawler/
+)
+
+
+
+IF "%1" == "" timeout /t 20
