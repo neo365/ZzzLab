440a53d923a03dbe75f235f3788a271d5e14a764
neo365 <neo365@gmail.com>
2023-03-10 14:54:28 +0900

    Auto Build: 0.2123.1320.2450

--- Framework/ZzzLab.Core/src/Net/Http/HttpClientEx.cs
+++ Framework/ZzzLab.Core/src/Net/Http/HttpClientEx.cs
@@ -1,82 +1,120 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace ZzzLab.Net.Http
 {
-    public class HttpClientHelper
+    public class HttpClientEx
     {
+        private HttpClient Client { set; get; }
+
+        public HttpClientEx(string baseUrl = null, string userAgent = null)
+        {
+            HttpClient client = new HttpClient();
+
+            if (string.IsNullOrWhiteSpace(baseUrl) == false)
+            {
+                client.BaseAddress = new Uri(baseUrl);
+            }
+            if (string.IsNullOrWhiteSpace(userAgent)) userAgent = "ZzzLab Agent 0.1";
+
+            client.DefaultRequestHeaders.Add("user-agent", userAgent);
+        }
+
+        internal static HttpClientEx Create(string baseUrl = null, string userAgent = null)
+            => new HttpClientEx(baseUrl, userAgent);
+
         /// <summary>
         /// Http Get 으로 문자열을 가져온다.
         /// </summary>
         /// <param name="url">접속url</param>
+        /// <param name="parameters">파라미터</param>
         /// <returns></returns>
         /// <exception cref="ArgumentNullException">url이 없을 경우</exception>
-        public static async Task<string> GetStringAsync(string url)
+        public async Task<HttpResponseMessage> GetAsync(string url, IEnumerable<KeyValuePair<string, string>> parameters = null)
         {
             if (string.IsNullOrWhiteSpace(url)) throw new ArgumentNullException(nameof(url));
-            using (HttpClient client = new HttpClient())
+
+            if (parameters != null && parameters.Any())
             {
-                using (var response = await client.GetAsync(url))
-                {
-                    response.EnsureSuccessStatusCode();
-                    return await response.Content.ReadAsStringAsync();
-                }
+                string queryString = (url.LastIndexOf('?') > 0 ? "&" : "?") + ToQueryString(parameters);
+                url = $"{url}{queryString}";
             }
+
+            HttpResponseMessage res = await this.Client.GetAsync(url);
+
+            return res;
         }
 
-        /// <summary>
-        /// Http Get 으로 문자열을 가져온다.
-        /// </summary>
-        /// <param name="url">접속url</param>
-        /// <param name="throwOnError"></param>
-        /// <returns></returns>
-        public static string GetString(string url, bool throwOnError = true)
-        {
-            if (throwOnError) return Task.Run(async () => await GetStringAsync(url)).Result;
+        public HttpResponseMessage Get(string url, IEnumerable<KeyValuePair<string, string>> parameters = null)
+            => Task.Run(async () => await GetAsync(url, parameters)).Result;
 
-            try
-            {
-                return Task.Run(async () => await GetStringAsync(url)).Result;
-            }
-            catch (Exception ex)
-            {
-                Logger.Error(ex);
-            }
-
-            return null;
-        }
-
-        /// <summary>
-        /// Http Get 으로 binary를 가져온다.
-        /// </summary>
-        /// <param name="url"></param>
-        /// <returns></returns>
-        /// <exception cref="ArgumentNullException">url이 없을 경우</exception>
-        public static async Task<byte[]> GetBytesAsync(string url)
+        public async Task<HttpResponseMessage> PostAsync(string url, IEnumerable<KeyValuePair<string, string>> parameters = null)
         {
             if (string.IsNullOrWhiteSpace(url)) throw new ArgumentNullException(nameof(url));
-            using (HttpClient client = new HttpClient())
+
+            HttpContent content = null;
+
+            if (parameters != null && parameters.Any())
             {
-                using (var response = await client.GetAsync(url))
+                string queryString = "";
+                foreach (var item in parameters)
                 {
-                    response.EnsureSuccessStatusCode();
-                    return await response.Content.ReadAsByteArrayAsync();
+                    queryString = $"&{HttpUtility.UrlEncode(item.Key)}={HttpUtility.UrlEncode(item.Value)}";
                 }
+                queryString = queryString.TrimStart('&');
+
+                content = new StringContent(queryString);
+                content.Headers.ContentType.MediaType = "application/x-www-form-urlencoded";
             }
+
+            HttpResponseMessage res = await this.Client.PostAsync(url, content);
+            res.EnsureSuccessStatusCode();
+
+            return res;
         }
 
-        public static byte[] GetBytes(string url, bool throwOnError = true)
-        {
-            if (throwOnError) return Task.Run(async () => await GetBytesAsync(url)).Result;
+        public HttpResponseMessage Post(string url, IEnumerable<KeyValuePair<string, string>> parameters = null)
+            => Task.Run(async () => await PostAsync(url, parameters)).Result;
 
-            try
+        public async Task<HttpResponseMessage> PostAsync(string url, string value, string mediaType = "application/text")
+        {
+            if (string.IsNullOrWhiteSpace(url)) throw new ArgumentNullException(nameof(url));
+
+            StringContent content = new StringContent(value);
+            content.Headers.ContentType.MediaType = mediaType;
+
+            HttpResponseMessage response = await this.Client.PostAsync(url, content);
+            return response;
+        }
+
+        public HttpResponseMessage Post(string url, string value, string mediaType = "application/text")
+            => Task.Run(async () => await PostAsync(url, value, mediaType)).Result;
+
+        public async Task<HttpResponseMessage> PostAsync<T>(string url, T value)
+        {
+            if (string.IsNullOrWhiteSpace(url)) throw new ArgumentNullException(nameof(url));
+
+            HttpResponseMessage response = await this.Client.PostAsync(url, JsonContent.Create(value));
+            return response;
+        }
+
+        public HttpResponseMessage Post<T>(string url, T value)
+            => Task.Run(async () => await PostAsync(url, value)).Result;
+
+        private string ToQueryString(IEnumerable<KeyValuePair<string, string>> parameters)
+        {
+            if (parameters != null && parameters.Any())
             {
-                return Task.Run(async () => await GetBytesAsync(url)).Result;
-            }
-            catch (Exception ex)
-            {
-                Logger.Error(ex);
+                string queryString = "";
+                foreach (var item in parameters)
+                {
+                    queryString = $"&{HttpUtility.UrlEncode(item.Key)}={HttpUtility.UrlEncode(item.Value)}";
+                }
+                return queryString.TrimStart('&');
             }
 
             return null;
