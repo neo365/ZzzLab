cf93f4c78a8665a61272aec6a7a34f669beeabff
neo365 <neo365@gmail.com>
2023-03-15 16:56:43 +0900

    Auto Build: 0.2123.1325.2651

--- Framework/ZzzLab.Core/src/Extension/ConvertExtension.Type.cs
+++ Framework/ZzzLab.Core/src/Extension/ConvertExtension.Type.cs
@@ -163,9 +163,7 @@ namespace ZzzLab
         {
             T result = ToEnumNullable<T>(obj);
 
-            if (result == null) throw new InvalidCastException();
-
-            return result;
+            return result == null ? throw new InvalidCastException() : result;
         }
 
 #endif
@@ -217,6 +215,19 @@ namespace ZzzLab
             using (MemoryStream ms = new MemoryStream())
             {
                 stream.CopyTo(ms);
+                return ms.ToArray();
+            }
+        }
+
+        public static byte[] ToBytes(this BinaryReader reader)
+        {
+            const int bufferSize = 4096;
+            using (var ms = new MemoryStream())
+            {
+                byte[] buffer = new byte[bufferSize];
+                int count;
+                while ((count = reader.Read(buffer, 0, buffer.Length)) != 0)
+                    ms.Write(buffer, 0, count);
                 return ms.ToArray();
             }
         }
