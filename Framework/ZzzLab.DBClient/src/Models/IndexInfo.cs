55e6748908e5512bc01e1f9592895b0e2f28e8df
김영동 <neo365@gmail.com>
2024-08-27 17:10:03 +0900

    Patch

---
+++ Framework/ZzzLab.DBClient/src/Models/IndexInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace ZzzLab.Data.Models
+{
+    public class IndexInfo
+    {
+        public string Owner { get; set; }
+        public string TableName { get; set; }
+        public string IndexName { get; set; }
+        public IEnumerable<IndexColumn> Columns { get; set; }
+    }
+
+    public class IndexColumn
+    {
+        public int OederNo { get; set; }
+        public string ColumnName { get; set; }
+        public string ColumnNameRef { get; set; }
+        public string Descend { get; set; }
+    }
+}
