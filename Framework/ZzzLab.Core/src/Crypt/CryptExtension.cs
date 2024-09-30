da15e62cb7a5d12fcfc3398e580292bb3ecaae28
김영동 <neo365@gmail.com>
2024-09-30 16:56:24 +0900

    patch

---
+++ Framework/ZzzLab.Core/src/Crypt/CryptExtension.cs
@@ -0,0 +1,11 @@
+﻿namespace ZzzLab.Crypt
+{
+    public static class CryptExtension
+    {
+        public static string EncodeUrlSafeString(this string s)
+            => s.Replace("=", ",").Replace("+", "-").Replace("/", "_");
+
+        public static string DecodeUrlSafeString(this string s)
+            => s.Replace(",", "=").Replace("-", "+").Replace("_", "/");
+    }
+}
---
+++ Framework/ZzzLab.Core/src/Logger.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ZzzLab
+{
+    public static class Logger
+    {
+        internal delegate void LogWriterDelegate(object value, [CallerMemberName] string methodName = null);
+
+        internal static LogWriterDelegate DebugFunc;
+        internal static LogWriterDelegate InfoFunc;
+        internal static LogWriterDelegate WarningFunc;
+        internal static LogWriterDelegate ErrorFunc;
+        internal static LogWriterDelegate FatalFunc;
+
+        public static void Debug(object value, [CallerMemberName] string methodName = null)
+            => DebugFunc?.Invoke(value, methodName);
+
+        public static void Info(object value, [CallerMemberName] string methodName = null)
+            => InfoFunc?.Invoke(value, methodName);
+
+        public static void Warning(object value, [CallerMemberName] string methodName = null)
+            => WarningFunc?.Invoke(value, methodName);
+
+        public static void Error(object value, [CallerMemberName] string methodName = null)
+            => ErrorFunc?.Invoke(value, methodName);
+
+        public static void Fatal(object value, [CallerMemberName] string methodName = null)
+            => FatalFunc?.Invoke(value, methodName);
+    }
+}
